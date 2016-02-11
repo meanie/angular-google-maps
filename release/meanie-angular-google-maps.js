943928d593a649bf4b909ac84540eaca948b0c6c
Adam Buczynski <me@adambuczynski.com>
2016-02-11 18:52:09 +1300

    Simplify and clean up

--- release/meanie-angular-google-maps.js
+++ release/meanie-angular-google-maps.js
@@ -5,219 +5,126 @@
  * Copyright (c) 2016 Adam Buczynski <me@adambuczynski.com>
  * License: MIT
  */
-(function(window, angular, undefined) {'use strict';
+'use strict';
 
-/**
- * Module definition and dependencies
- */
-angular.module('Google.Maps.Api.Service', [])
+(function (window, angular, undefined) {
+  'use strict';
 
-/**
- * Wrapper for google maps API
- */
-.factory('GoogleMapsApi', ['$window', function($window) {
-  if (!$window.google || !$window.google.maps) {
-    throw new Error(
-      'Global `google` variable or `google.maps` missing.' +
-      'Make sure to include the relevant external Google script(s).'
-    );
-  }
-  return $window.google;
-}]);
+  /**
+   * Module definition and dependencies
+   */
 
+  angular.module('Google.Maps.Api.Service', [])
+
+  /**
+   * Wrapper for google maps API
+   */
+  .factory('GoogleMapsApi', ['$window', function ($window) {
+    if (!$window.google || !$window.google.maps) {
+      throw new Error('Global `google` variable or `google.maps` missing.' + 'Make sure to include the relevant external Google script(s).');
+    }
+    return $window.google;
+  }]);
 })(window, window.angular);
+'use strict';
 
-(function(window, angular, undefined) {'use strict';
-
-/**
- * See: https://developers.google.com/maps/documentation/javascript/places-autocomplete
- *      https://github.com/kuhnza/angular-google-places-autocomplete/blob/master/src/autocomplete.js
- */
-
-/**
- * Module definition and dependencies
- */
-angular.module('Google.Maps.PlacesAutocomplete.Directive', [
-  'Google.Maps.Api.Service'
-])
-
-/**
- * Directive
- */
-.directive('placesAutocomplete', ['GoogleMapsApi', '$injector', '$timeout', function(
-  Google, $injector, $timeout
-) {
+(function (window, angular, undefined) {
+  'use strict';
 
   /**
-   * Convert string to camel case
+   * Module definition and dependencies
    */
-  function toCamelCase(str) {
-    return str
-      .replace(/_+|\-+/g, ' ')
-      .replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
-        if (+match === 0) {
-          return '';
-        }
-        return (index === 0) ? match.toLowerCase() : match.toUpperCase();
-      });
-  }
 
-  /**
-   * Convert object keys to camel case
-   */
-  var keysToCamelCase = function(obj) {
-    var newObj = {};
-    for (var key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        var newKey = toCamelCase(key);
-        newObj[newKey] = angular.copy(obj[key]);
-      }
-    }
-    return newObj;
-  };
-
-  //Check if we have the convert library available to us and us that instead
-  if ($injector.has('$convert')) {
-    keysToCamelCase = $injector.get('$convert').object.keysToCamelCase;
-  }
-
-  /**
-   * Helper to find an address component type
-   */
-  function findAddressComponentOfType(place, type, format) {
-    format = format || 'long_name';
-    for (var c = 0; c < place.addressComponents.length; c++) {
-      if (place.addressComponents[c].types.indexOf(type) !== -1) {
-        return place.addressComponents[c][format];
-      }
-    }
-    return '';
-  }
-
-  /**
-   * Determine spread of a place, (e.g. for inaccurate addresses, like a suburb or city)
-   */
-  function determineSpread(place) {
-
-    //Validate viewport data
-    if (!place.geometry.viewport) {
-      return 0;
-    }
-
-    //Get distance
-    var distance = Google.maps.geometry.spherical.computeDistanceBetween(
-      place.geometry.viewport.getNorthEast(), place.geometry.viewport.getSouthWest()
-    );
-
-    //Return rounded to meters
-    return Math.round(distance * 1000) / 1000;
-  }
-
-  /**
-   * Round lat/long values
-   */
-  function roundLatLng(num) {
-    return Math.round(num * 10000000) / 10000000;
-  }
+  angular.module('Google.Maps.PlacesAutocomplete.Directive', ['Google.Maps.Api.Service'])
 
   /**
    * Directive
    */
-  return {
-    restrict: 'A',
-    scope: {
-      place: '=placeModel',
-      address: '=addressModel',
-      geoLocation: '=geoLocation'
-    },
-    link: function(scope, element, attrs) {
-
-      //Autocomplete options
-      var options = {};
-
-      //Turn off field's autocomplete
-      //NOTE: Setting to merely 'off' doesn't do the trick
-      //See: https://developer.mozilla.org/en-US/docs/Web/Security/
-      //     Securing_your_site/Turning_off_form_autocompletion
-      //In addition, this needs to be wrapped in a timeout because Google Maps sets it to off
-      $timeout(function() {
-        element.attr('autocomplete', 'nope');
-      }, 500);
-
-      //Restrict by types?
-      if (attrs.restrictTypes) {
-        options.types = attrs.restrictTypes.split(',');
-      }
-
-      //Restrict by country?
-      if (attrs.restrictCountry) {
-        options.componentRestrictions = options.componentRestrictions || {};
-        options.componentRestrictions.country = attrs.restrictCountry;
-      }
-
-      //Initialize autocomplete API now with options
-      var autocomplete = new Google.maps.places.Autocomplete(element[0], options);
-
-      //Set bounds if geo location given
-      if (scope.geoLocation && angular.isObject(scope.geoLocation) && scope.geoLocation.coords) {
-        var circle = new Google.maps.Circle({
-          radius: scope.geoLocation.coords.accuracy,
-          center: new Google.maps.LatLng(
-            scope.geoLocation.coords.latitude, scope.geoLocation.coords.longitude
-          )
-        });
-        autocomplete.setBounds(circle.getBounds());
-      }
+  .directive('placesAutocomplete', ['GoogleMapsApi', function (Google) {
+    return {
+      restrict: 'A',
+      scope: {
+        geoLocation: '=',
+        options: '=',
+        onChange: '&'
+      },
 
       /**
-       * Place changed handler
+       * Controller
        */
-      function placeChanged() {
+      controller: ['$scope', '$attrs', '$element', '$timeout', '$convert', function ($scope, $attrs, $element, $timeout, $convert) {
 
-        //Get selected place and convert keys
-        var place = keysToCamelCase(autocomplete.getPlace());
+        //Set options
+        $scope.options = $scope.options || {};
 
-        //Set in scope
-        scope.$apply(function() {
+        //Turn off field's autocomplete
+        //NOTE: Setting to merely 'off' doesn't do the trick
+        //See: https://developer.mozilla.org/en-US/docs/Web/Security/
+        //     Securing_your_site/Turning_off_form_autocompletion
+        //This needs to be wrapped in a timeout because Google Maps sets it to off.
+        $timeout(function () {
+          $element.attr('autocomplete', 'nope');
+        }, 500);
 
-          //Set entire place in scope model
-          scope.place = place;
-
-          //Set specific filtered address details in scope
-          scope.address = scope.address || {};
-          scope.address.selected = true;
-          scope.address.enteredValue = element[0].value;
-          scope.address.spread = determineSpread(place);
-          scope.address.latitude = roundLatLng(place.geometry.location.lat());
-          scope.address.longitude = roundLatLng(place.geometry.location.lng());
-          scope.address.vicinity = place.vicinity;
-          scope.address.streetNumber = findAddressComponentOfType(place, 'street_number');
-          scope.address.streetName = findAddressComponentOfType(place, 'route');
-          scope.address.city = findAddressComponentOfType(place, 'locality');
-          scope.address.postalCode = findAddressComponentOfType(place, 'postal_code');
-        });
-      }
+        /**
+         * Place changed handler
+         */
+        $scope.placeChanged = function (place) {
+          if ($attrs.onChange) {
+            $scope.onChange({
+              place: $convert.object.keysToCamelCase(place)
+            });
+          }
+        };
+      }],
 
       /**
-       * Event listener for place changes
+       * Linking function
        */
-      var listener = Google.maps.event.addListener(
-        autocomplete, 'place_changed', placeChanged
-      );
+      link: function link(scope, element) {
 
-      /**
-       * Event listener for scope destruction
-       */
-      scope.$on('$destroy', function() {
-        Google.maps.event.removeListener(listener);
-        Google.maps.event.clearInstanceListeners(autocomplete);
-        var containers = document.getElementsByClassName('pac-container');
-        for (var i = 0; i < containers.length; i++) {
-          containers[i].parentNode.removeChild(containers[i]);
+        //Initialize autocomplete API now with options
+        var autocomplete = new Google.maps.places.Autocomplete(element[0], scope.options);
+
+        //Set bounds if geo location given
+        if (scope.geoLocation && angular.isObject(scope.geoLocation) && scope.geoLocation.coords) {
+          var circle = new Google.maps.Circle({
+            radius: scope.geoLocation.coords.accuracy,
+            center: new Google.maps.LatLng(scope.geoLocation.coords.latitude, scope.geoLocation.coords.longitude)
+          });
+          autocomplete.setBounds(circle.getBounds());
         }
-      });
-    }
-  };
-}]);
 
+        /**
+         * Place changed handler
+         */
+        function placeChanged() {
+
+          //Get selected place and convert keys
+          var place = autocomplete.getPlace();
+          place.inputValue = element[0].value;
+
+          //Set in scope
+          scope.$apply(function () {
+            scope.placeChanged(place);
+          });
+        }
+
+        /**
+         * Event listener for place changes
+         */
+        var listener = Google.maps.event.addListener(autocomplete, 'place_changed', placeChanged);
+
+        //Event listener for scope destruction
+        scope.$on('$destroy', function () {
+          Google.maps.event.removeListener(listener);
+          Google.maps.event.clearInstanceListeners(autocomplete);
+          var containers = document.getElementsByClassName('pac-container');
+          for (var i = 0; i < containers.length; i++) {
+            containers[i].parentNode.removeChild(containers[i]);
+          }
+        });
+      }
+    };
+  }]);
 })(window, window.angular);
--- src/placesAutocomplete.directive.js
+++ src/placesAutocomplete.directive.js
@@ -1,10 +1,5 @@
 
 /**
- * See: https://developers.google.com/maps/documentation/javascript/places-autocomplete
- *      https://github.com/kuhnza/angular-google-places-autocomplete/blob/master/src/autocomplete.js
- */
-
-/**
  * Module definition and dependencies
  */
 angular.module('Google.Maps.PlacesAutocomplete.Directive', [
@@ -14,123 +9,57 @@ angular.module('Google.Maps.PlacesAutocomplete.Directive', [
 /**
  * Directive
  */
-.directive('placesAutocomplete', ['GoogleMapsApi', '$injector', '$timeout', function(
-  Google, $injector, $timeout
-) {
-
-  /**
-   * Convert string to camel case
-   */
-  function toCamelCase(str) {
-    return str
-      .replace(/_+|\-+/g, ' ')
-      .replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
-        if (+match === 0) {
-          return '';
-        }
-        return (index === 0) ? match.toLowerCase() : match.toUpperCase();
-      });
-  }
-
-  /**
-   * Convert object keys to camel case
-   */
-  var keysToCamelCase = function(obj) {
-    var newObj = {};
-    for (var key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        var newKey = toCamelCase(key);
-        newObj[newKey] = angular.copy(obj[key]);
-      }
-    }
-    return newObj;
-  };
-
-  //Check if we have the convert library available to us and us that instead
-  if ($injector.has('$convert')) {
-    keysToCamelCase = $injector.get('$convert').object.keysToCamelCase;
-  }
-
-  /**
-   * Helper to find an address component type
-   */
-  function findAddressComponentOfType(place, type, format) {
-    format = format || 'long_name';
-    for (var c = 0; c < place.addressComponents.length; c++) {
-      if (place.addressComponents[c].types.indexOf(type) !== -1) {
-        return place.addressComponents[c][format];
-      }
-    }
-    return '';
-  }
-
-  /**
-   * Determine spread of a place, (e.g. for inaccurate addresses, like a suburb or city)
-   */
-  function determineSpread(place) {
-
-    //Validate viewport data
-    if (!place.geometry.viewport) {
-      return 0;
-    }
-
-    //Get distance
-    var distance = Google.maps.geometry.spherical.computeDistanceBetween(
-      place.geometry.viewport.getNorthEast(), place.geometry.viewport.getSouthWest()
-    );
-
-    //Return rounded to meters
-    return Math.round(distance * 1000) / 1000;
-  }
-
-  /**
-   * Round lat/long values
-   */
-  function roundLatLng(num) {
-    return Math.round(num * 10000000) / 10000000;
-  }
-
-  /**
-   * Directive
-   */
+.directive('placesAutocomplete', ['GoogleMapsApi', function(Google) {
   return {
     restrict: 'A',
     scope: {
-      place: '=placeModel',
-      address: '=addressModel',
-      geoLocation: '=geoLocation'
+      geoLocation: '=',
+      options: '=',
+      onChange: '&'
     },
-    link: function(scope, element, attrs) {
 
-      //Autocomplete options
-      var options = {};
+    /**
+     * Controller
+     */
+    controller: function($scope, $attrs, $element, $timeout, $convert) {
+
+      //Set options
+      $scope.options = $scope.options || {};
 
       //Turn off field's autocomplete
       //NOTE: Setting to merely 'off' doesn't do the trick
       //See: https://developer.mozilla.org/en-US/docs/Web/Security/
       //     Securing_your_site/Turning_off_form_autocompletion
-      //In addition, this needs to be wrapped in a timeout because Google Maps sets it to off
+      //This needs to be wrapped in a timeout because Google Maps sets it to off.
       $timeout(function() {
-        element.attr('autocomplete', 'nope');
+        $element.attr('autocomplete', 'nope');
       }, 500);
 
-      //Restrict by types?
-      if (attrs.restrictTypes) {
-        options.types = attrs.restrictTypes.split(',');
-      }
+      /**
+       * Place changed handler
+       */
+      $scope.placeChanged = function(place) {
+        if ($attrs.onChange) {
+          $scope.onChange({
+            place: $convert.object.keysToCamelCase(place)
+          });
+        }
+      };
+    },
 
-      //Restrict by country?
-      if (attrs.restrictCountry) {
-        options.componentRestrictions = options.componentRestrictions || {};
-        options.componentRestrictions.country = attrs.restrictCountry;
-      }
+    /**
+     * Linking function
+     */
+    link: function(scope, element) {
 
       //Initialize autocomplete API now with options
-      var autocomplete = new Google.maps.places.Autocomplete(element[0], options);
+      let autocomplete = new Google.maps.places.Autocomplete(
+        element[0], scope.options
+      );
 
       //Set bounds if geo location given
       if (scope.geoLocation && angular.isObject(scope.geoLocation) && scope.geoLocation.coords) {
-        var circle = new Google.maps.Circle({
+        let circle = new Google.maps.Circle({
           radius: scope.geoLocation.coords.accuracy,
           center: new Google.maps.LatLng(
             scope.geoLocation.coords.latitude, scope.geoLocation.coords.longitude
@@ -145,44 +74,28 @@ angular.module('Google.Maps.PlacesAutocomplete.Directive', [
       function placeChanged() {
 
         //Get selected place and convert keys
-        var place = keysToCamelCase(autocomplete.getPlace());
+        let place = autocomplete.getPlace();
+        place.inputValue = element[0].value;
 
         //Set in scope
-        scope.$apply(function() {
-
-          //Set entire place in scope model
-          scope.place = place;
-
-          //Set specific filtered address details in scope
-          scope.address = scope.address || {};
-          scope.address.selected = true;
-          scope.address.enteredValue = element[0].value;
-          scope.address.spread = determineSpread(place);
-          scope.address.latitude = roundLatLng(place.geometry.location.lat());
-          scope.address.longitude = roundLatLng(place.geometry.location.lng());
-          scope.address.vicinity = place.vicinity;
-          scope.address.streetNumber = findAddressComponentOfType(place, 'street_number');
-          scope.address.streetName = findAddressComponentOfType(place, 'route');
-          scope.address.city = findAddressComponentOfType(place, 'locality');
-          scope.address.postalCode = findAddressComponentOfType(place, 'postal_code');
+        scope.$apply(() => {
+          scope.placeChanged(place);
         });
       }
 
       /**
        * Event listener for place changes
        */
-      var listener = Google.maps.event.addListener(
+      let listener = Google.maps.event.addListener(
         autocomplete, 'place_changed', placeChanged
       );
 
-      /**
-       * Event listener for scope destruction
-       */
-      scope.$on('$destroy', function() {
+      //Event listener for scope destruction
+      scope.$on('$destroy', () => {
         Google.maps.event.removeListener(listener);
         Google.maps.event.clearInstanceListeners(autocomplete);
-        var containers = document.getElementsByClassName('pac-container');
-        for (var i = 0; i < containers.length; i++) {
+        let containers = document.getElementsByClassName('pac-container');
+        for (let i = 0; i < containers.length; i++) {
           containers[i].parentNode.removeChild(containers[i]);
         }
       });
