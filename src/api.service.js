24de0244e539083bc32f9a4c576bed9ff3a3e362
Adam Buczynski <me@adambuczynski.com>
2016-02-11 16:23:35 +1300

    Check for actual google maps API

--- src/api.service.js
+++ src/api.service.js
@@ -8,10 +8,10 @@ angular.module('Google.Maps.Api.Service', [])
  * Wrapper for google maps API
  */
 .factory('GoogleMapsApi', function($window) {
-  if (!$window.google) {
+  if (!$window.google || !$window.google.maps) {
     throw new Error(
-      'Global `google` variable missing. Make sure to include the relevant external ' +
-      'Google script(s).'
+      'Global `google` variable or `google.maps` missing.' +
+      'Make sure to include the relevant external Google script(s).'
     );
   }
   return $window.google;
