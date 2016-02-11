57f2e34db7a89bec9dc1c959e9b0fa83809b2e2b
Adam Buczynski <me@adambuczynski.com>
2016-02-11 18:54:33 +1300

    Google maps address auto complete
    Close #189

--- release/meanie-angular-google-maps.js
+++ release/meanie-angular-google-maps.js
@@ -1,5 +1,5 @@
 /**
- * meanie-angular-google-maps - v1.2.2 - 11-1-2016
+ * meanie-angular-google-maps - v1.3.0 - 11-1-2016
  * https://github.com/meanie/
  *
  * Copyright (c) 2016 Adam Buczynski <me@adambuczynski.com>
