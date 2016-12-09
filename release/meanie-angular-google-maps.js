56db2edcc58a1970b279155ff4f8e56afbf249f6
Adam Reis <adambuczynski@gmail.com>
2016-12-09 21:09:22 +1300

    1.4.1

--- release/meanie-angular-google-maps.js
+++ release/meanie-angular-google-maps.js
@@ -1,7 +1,7 @@
 /**
  * meanie-angular-google-maps * https://github.com/meanie/
  *
- * Copyright (c) 2016 Adam Buczynski <me@adambuczynski.com>
+ * Copyright (c) 2016 Adam Reis <adam@reis.nz>
  * License: MIT
  */
 (function (window, angular, undefined) {
